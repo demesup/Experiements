127191c39eb498ed4097c53a1ceb60328ed7a4a8
demesup <guzhansofia@gmail.com>
2022-06-08 17:18:32 +0300

    JavaJunior tasks

---
+++ src/top20tasks/PrimeNumbers.java
@@ -0,0 +1,39 @@
+package top20tasks;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        primeNumbersTo997Table();
+        System.out.println();
+        isPrimeNumber(39);
+    }
+
+    private static void isPrimeNumber(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) count++;
+        }
+        if (count > 2)
+            System.out.println(number + " is not a prime number");
+        else System.out.println(number + " is a prime number");
+    }
+
+    private static void primeNumbersTo997Table() {
+        int numberCount = 0;
+        for (int i = 0; i < 998; i++) {
+            if (i == 2 || i == 1) {
+                System.out.print(i + " ");
+                numberCount++;
+                continue;
+            }
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+                System.out.print(i + " ");
+                numberCount++;
+            }
+            if (numberCount == 12) {
+                System.out.println();
+                numberCount = 0;
+            }
+        }
+    }
+}
---
+++ src/top20tasks/ReversedString.java
@@ -0,0 +1,31 @@
+package top20tasks;
+
+public class ReversedString {
+    public static void main(String[] args) {
+        reversedString("Hello! You need to reverse me)");
+        reversedCharacters1("!@#$%^&*()_+");
+        reversedCharacters2( "!@#$%^&*()_+");
+    }
+
+    private static void reversedCharacters2(String str) {
+        String[] str1 = str.split("");
+        for (int i = str1.length - 1; i >= 0; i--) {
+            System.out.print(str1[i]);
+        }
+        System.out.println();
+    }
+
+    private static void reversedCharacters1(String str) {
+        char[] chars = str.toCharArray();
+        for (int i = chars.length - 1; i > -1; i--) {
+            System.out.print(chars[i]);
+        }
+        System.out.println();
+    }
+
+    private static void reversedString(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(str);
+        System.out.println(stringBuilder.reverse());
+    }
+}
