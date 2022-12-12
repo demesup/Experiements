7ab717ba93a4ebe3354864346231a183d9eb702c
Sofiya <guzhansofia@gmail.com>
2022-12-12 20:39:35 +0200

    * For example:    * | fromAmount  | result           |    * |-------------|------------------|    * | 0.006       | XRP->USDT:0.01   |    * | 0.06        | XRP->USDT:0.1    |    * | 0.6         | XRP->USDT:1      |    * | 6           | XRP->USDT:10     |    * | 42          | XRP->USDT:100    |    * | 102         | XRP->USDT:1000   |    * | 2 433       | XRP->USDT:10000  |    * | 55 661      | XRP->USDT:100000 |

---
+++ src.main/java/algorithmsAndDataStructures/Parser.java
@@ -0,0 +1,33 @@
+package algorithmsAndDataStructures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+public class Parser {
+    public static void main(String[] args) throws IOException {
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+        String i;
+        while (!(i = scanner.readLine()).isEmpty()) {
+            System.out.println(parse(i));
+        }
+    }
+
+    private static String parse(String str) {
+        if (Pattern.compile("^(?:(?!\\.)0\\.0*1|10*)$").matcher(str).matches()) return str;
+        try {
+            Integer.parseInt(str);
+            return "1" + str.replaceAll("\\d", "0");
+        } catch (Exception i) {
+            try {
+                Float.parseFloat(str);
+                if (str.length() == 3) return "1";
+                return str.substring(0, str.lastIndexOf("0")).replaceAll("\\d", "0") + "1";
+            } catch (Exception e) {
+                return "Wrong number";
+            }
+        }
+    }
+
+}
